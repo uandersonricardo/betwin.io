5dde611a93e1ec6657e01c1512edb5d18c0c2c3d
Uanderson Ricardo <uandersonrfs@gmail.com>
2022-08-21 10:14:05 -0300

    user-agent and accept headers for web scraping

--- backend/src/business/controls/MatchControl.ts
+++ backend/src/business/controls/MatchControl.ts
@@ -30,7 +30,15 @@ class MatchControl {
 
   public async matches(filter: string) {
     const { data } = await axios.get(
-      `https://eu-offering.kambicdn.org/offering/v2018/ub/listView${filter}.json?lang=pt_BR&market=BR`
+      `https://eu-offering.kambicdn.org/offering/v2018/ub/listView${filter}.json?lang=pt_BR&market=BR`,
+      {
+        headers: {
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+        }
+      }
     );
 
     const groupByCompetitions = (events: any[]) => {
@@ -122,7 +130,15 @@ class MatchControl {
 
   public async match(matchId: string) {
     const { data } = await axios.get(
-      `https://eu-offering.kambicdn.org/offering/v2018/ub/betoffer/event/${matchId}.json?lang=pt_BR`
+      `https://eu-offering.kambicdn.org/offering/v2018/ub/betoffer/event/${matchId}.json?lang=pt_BR`,
+      {
+        headers: {
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+        }
+      }
     );
     const groupByCategory = (data: any) => {
       const categories: any[] = [];
@@ -144,7 +160,15 @@ class MatchControl {
     };
 
     const { data: liveData } = await axios.get(
-      `https://eu-offering.kambicdn.org/offering/v2018/ub/event/${matchId}/livedata.json?lang=pt_BR&market=BR`
+      `https://eu-offering.kambicdn.org/offering/v2018/ub/event/${matchId}/livedata.json?lang=pt_BR&market=BR`,
+      {
+        headers: {
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+        }
+      }
     );
 
     const oddsMatch: MatchInfo = {
