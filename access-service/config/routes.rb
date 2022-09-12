4e55618dbaa155e560c4f5d4b99f98dbefc26862
Uanderson Ricardo <uandersonrfs@gmail.com>
2022-09-11 23:10:58 -0300

    consul as discovery server

--- access-service/config/routes.rb
+++ access-service/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post '/register', to: 'access#register'
   get '/me', to: 'access#me'
 
+  get '/health', to: 'application#health'
+
   get '/*a', to: 'application#not_found'
 end
